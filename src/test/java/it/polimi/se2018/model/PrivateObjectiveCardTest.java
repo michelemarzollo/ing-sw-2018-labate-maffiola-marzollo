e8ffa5eb622b8fef64c881091c6b4c62164fd9aa
dvdmff <davide.maffiola@mail.polimi.it>
2018-05-13 16:13:15 +0200

    Add unit tests for objective cards
    The tests and their respective documentation for all objective cards
    (both private and public) and their factories have been added.

---
+++ src/test/java/it/polimi/se2018/model/PrivateObjectiveCardTest.java
@@ -0,0 +1,65 @@
+package it.polimi.se2018.model;
+
+import it.polimi.se2018.utils.GridUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Random;
+
+/**
+ * Unit tests for PrivateObjectiveCard class.
+ */
+public class PrivateObjectiveCardTest {
+
+    /**
+     * Tests if the object is correctly instantiated and initialized.
+     */
+    @Test
+    public void testConstructor() {
+        String name = "TestCard";
+        Colour colour = Colour.BLUE;
+        PrivateObjectiveCard card = new PrivateObjectiveCard(name, colour);
+        Assert.assertEquals(name, card.getName());
+        Assert.assertEquals(colour, card.getColour());
+    }
+
+    /**
+     * Tests if the score is calculated correctly in a simple case.
+     * <p>In this simple case, the gird contains five dice of value 4
+     * with the same colour as the private objective card.</p>
+     */
+    @Test
+    public void testSimpleScoreCalculation() {
+        Cell[][] grid = GridUtils.getEmptyUnrestrictedGrid(4, 5);
+        Die redDie = new Die(4, new Random(), Colour.RED);
+
+        try {
+            grid[0][0].place(redDie);
+            grid[1][1].place(redDie);
+            grid[2][2].place(redDie);
+            grid[3][3].place(redDie);
+            grid[0][4].place(redDie);
+        } catch (PlacementErrorException e) {
+            Assert.fail();
+        }
+
+        PrivateObjectiveCard card = new PrivateObjectiveCard("Test", Colour.RED);
+        int actualScore = card.getScore(grid);
+        int expectedScore = 4 * 5;
+
+        Assert.assertEquals(expectedScore, actualScore);
+    }
+
+    /**
+     * Tests if the score is calculated correctly on an empty grid.
+     */
+    @Test
+    public void testEmptyGridScore() {
+        Cell[][] grid = GridUtils.getEmptyUnrestrictedGrid(4, 5);
+
+        PrivateObjectiveCard card = new PrivateObjectiveCard("Test", Colour.GREEN);
+        int actualScore = card.getScore(grid);
+
+        Assert.assertEquals(0, actualScore);
+    }
+}
---
+++ src/test/java/it/polimi/se2018/model/PrivateObjectiveFactoryTest.java
@@ -0,0 +1,60 @@
+package it.polimi.se2018.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * Unit tests for PrivateObjectiveFactory class.
+ */
+public class PrivateObjectiveFactoryTest {
+
+    /**
+     * Tests if the factory returns the correct number of instances.
+     */
+    @Test
+    public void testCorrectNumberOfInstances() {
+        int instances = 4;
+        PrivateObjectiveFactory factory = new PrivateObjectiveFactory();
+        PrivateObjectiveCard[] cards = factory.newInstances(instances);
+
+        long actualInstances = cards.length;
+
+        Assert.assertEquals(instances, actualInstances);
+
+    }
+
+    /**
+     * Tests if the factory returns a set of unique cards.
+     */
+    @Test
+    public void testNoCardRepetition() {
+        int instances = 5;
+        PrivateObjectiveFactory factory = new PrivateObjectiveFactory();
+        PrivateObjectiveCard[] cards = factory.newInstances(instances);
+
+        long distinctCards = Arrays.stream(cards)
+                .map(PrivateObjectiveCard::getName)
+                .distinct()
+                .count();
+
+        Assert.assertEquals(instances, distinctCards);
+    }
+
+    /**
+     * Tests if the factory throws IllegalArgumentException if too
+     * many instances are requested.
+     */
+    @Test
+    public void testRequestTooManyCards() {
+        int instances = 20;
+        PrivateObjectiveFactory factory = new PrivateObjectiveFactory();
+        try {
+            PrivateObjectiveCard[] cards = factory.newInstances(instances);
+            Assert.fail();
+        } catch (IllegalArgumentException ignored) {
+
+        }
+    }
+}
