717f9be33ddf4c2608f3d0a9934a466f8eb01071
dvdmff <davide.maffiola@mail.polimi.it>
2018-05-17 18:46:17 +0200

    Add documentation for ModelEvent and PlayerConnection

--- src/main/java/it/polimi/se2018/model/events/ModelEvent.java
+++ src/main/java/it/polimi/se2018/model/events/ModelEvent.java
@@ -1,15 +1,29 @@
 package it.polimi.se2018.model.events;
 
+/**
+ * This enum represent the possible types of model updates.
+ */
 public enum ModelEvent {
     NEXT_TURN("NEXT"), GAME_SETUP("GAME-SETUP"), GAME_END("END"), PATTERN_SELECTION("SELECTION"),
     MOVE_UPDATE("MOVE"), ROUND_SETUP("ROUND-SETUP"), PLAYER_CONNECTION("CONNECTION");
 
+    /**
+     * The label associated with the event type.
+     */
     private String label;
 
+    /**
+     * Creates a new enum entry with the given label.
+     * @param label The label of the event type.
+     */
     ModelEvent(String label) {
         this.label = label;
     }
 
+    /**
+     * Represents the enum entry as a string.
+     * @return A string containing the label of the event type.
+     */
     @Override
     public String toString() {
         return label;
--- src/main/java/it/polimi/se2018/model/events/PlayerConnection.java
+++ src/main/java/it/polimi/se2018/model/events/PlayerConnection.java
@@ -1,20 +1,45 @@
 package it.polimi.se2018.model.events;
 
+/**
+ * Event to update views about the change in the connection
+ * status of a player.
+ * @author dvdmff
+ */
 public class PlayerConnection extends ModelUpdate {
 
+    /**
+     * The name of the involved player.
+     */
     private final String playerName;
+    /**
+     * Flag to indicate if the player is connected.
+     */
     private final boolean connected;
 
+    /**
+     * Creates a new instance with the specified name and connection flag.
+     * @param playerName The name of the player who changed his connection status.
+     * @param connected Flag for the new connection status.
+     */
     public PlayerConnection(String playerName, boolean connected) {
         super(ModelEvent.PLAYER_CONNECTION);
         this.playerName = playerName;
         this.connected = connected;
     }
 
+    /**
+     * Getter for the involved player name.
+     * @return The name of the involved player.
+     */
     public String getPlayerName() {
         return playerName;
     }
 
+    /**
+     * Getter for the connection flag.
+     * @return {@code true} if the player is connected;
+     * {@code false} otherwise.
+     */
     public boolean isConnected() {
         return connected;
     }
