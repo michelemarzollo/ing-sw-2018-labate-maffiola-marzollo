46beac33995c62bb18f4a41a001158b7f98ca002
dvdmff <davide.maffiola@mail.polimi.it>
2018-05-22 14:07:53 +0200

    Refine abstract controller
    The abstract controller now deals only about what strictly concerns
    both game modalities.
    Documentation has been updated.

--- src/main/java/it/polimi/se2018/controller/ToolCardBehavior.java
+++ src/main/java/it/polimi/se2018/controller/ToolCardBehavior.java
@@ -3,7 +3,22 @@ package it.polimi.se2018.controller;
 import it.polimi.se2018.model.Game;
 import it.polimi.se2018.model.events.ViewMessage;
 
+/**
+ * This interface represents the behavior of a tool card.
+ */
 public interface ToolCardBehavior {
+
+    /**
+     * Selects the correct view to gather the parameters the tool card
+     * needs to be used.
+     * @param message The message sent by the view.
+     */
     void askParameters(ViewMessage message);
+
+    /**
+     * Applies the effect of the tool card to the game.
+     * @param game The game the effect has to be applied to.
+     * @param message The message sent by th view.
+     */
     void useToolCard(Game game, ViewMessage message);
 }
